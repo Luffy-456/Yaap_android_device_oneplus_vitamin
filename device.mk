1678c7745697a366b4ce8d8639a39b4a0605320e
Luffy-456 <anshuman567.in@gmail.com>
2025-06-15 22:26:07 +0530

    applied alert slider patch for aosp

--- device.mk
+++ device.mk
@@ -50,6 +50,11 @@ PRODUCT_PACKAGES += \
     KeyHandler \
     tri-state-key-calibrate
 
+#Alert slider UI
+PRODUCT_PACKAGES += \
+    POSFrameworksResTarget
+
+    
 # API
 BOARD_SHIPPING_API_LEVEL := 31
 PRODUCT_SHIPPING_API_LEVEL := 33
